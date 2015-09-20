e735ad01d383d61856a6a0afcb713de65761e2e5
torbas <t.orbasido@gmail.com>
2015-09-20 16:25:48 -0700

    create cards

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 $(function(){
+	var main = $("#main");
+
+	var card = $("<div>", {
+		"class": "col-md-2 card"
+	});
+
+	main.children(".hand").each(function(){
+		for(var i = 0; i < 5; i++){
+			card.clone().appendTo($(this));
+		}
+	});
 
 });
