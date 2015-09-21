cd09bf853306c698ab12e5641d9508757b751be3
torbas <t.orbasido@gmail.com>
2015-09-20 17:34:55 -0700

    add card styles

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 $(function(){
 	var main = $("#main");
 
-	var card = $("<div>", {
-		"class": "col-md-2 card"
+	var cardContainer = $("<div>", {
+		"class": "col-sm-2 card-container"
 	});
+
+	var card = $("<div>", {
+		"class": "col-sm-2 card"
+	});
+
 
 	main.children(".hand").each(function(){
 		for(var i = 0; i < 5; i++){
-			card.clone().appendTo($(this));
+			var tmpCard = card.clone().appendTo($(this));
+			if(i==0){
+				tmpCard.addClass("col-sm-offset-1");
+			}
 		}
 	});
+
+	// $(".row").on("click", ".card-container", function(){
+
+	// });
 
 });
