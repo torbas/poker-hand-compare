107e7114957e0ba2d3cf8c1ba970cebbe24d360e
torbas <t.orbasido@gmail.com>
2015-09-20 21:09:46 -0700

    add compare button and add suit img for possible addition

--- app.js
+++ app.js
@@ -17,6 +17,12 @@ $(function(){
 		"class": "col-sm-6 number-select"
 	});
 
+	var compareBtn = $("<button>",{
+		"class": "col-sm-12 btn btn-primary",
+		"id": "compare-btn",
+		"text": "Compare"
+	});
+
 	var suits = ["&spades;", "&hearts;", "&clubs;", "&diams;"];
 
 	//Jack 11, Queen 12, King 13, Ace 14
@@ -27,13 +33,13 @@ $(function(){
 
 	for(var s=0; s<suits.length; s++){
 		var option = $("<option>").html(suits[s]);
-		console.log(option);
 		option.appendTo(cardSuit);
 	}
 
 	cardSuit.appendTo(cardContainer);
 	cardNumber.appendTo(cardContainer);
 	card.appendTo(cardContainer);
+	compareBtn.appendTo(main);
 
 	main.children(".hand").each(function(){
 		for(var i = 0; i < 5; i++){
@@ -44,8 +50,8 @@ $(function(){
 		}
 	});
 
-	// $(".row").on("click", ".card-container", function(){
-
-	// });
+	$("#main").on("click", "#compare-btn", function(){
+		alert("hello");
+	});
 
 });
