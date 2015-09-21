2fc20844138e006432b1f4ebbc13fd1f1426701d
torbas <t.orbasido@gmail.com>
2015-09-20 22:58:01 -0700

    begin hand validation logic

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ $(function(){
 		pushArray: function(options){
 			var arr = [];
 			options.each(function(){
-				arr.push($(this).text());
+				arr.push($(this).val());
 			});
 			return arr;
 		},
@@ -23,7 +23,20 @@ $(function(){
 			}
 			
 			return !(count==5);
-		}
+		},
+		validatePairs: function(suits, number){
+		},
+		determineHand: function(suits, numbers){
+			var handArr = [];
+
+			for(var i=0; i<5; i++){
+				handArr.push(suits[i]+numbers[i]);
+			}
+
+			var hand = handArr.join(",");
+			console.log(hand);
+		},
+
 	};
 
 	var main = $("#main");
@@ -54,12 +67,44 @@ $(function(){
 
 	//Jack 11, Queen 12, King 13, Ace 14
 	for(var n=1; n<=14; n++){
-		var option = $("<option>").text(n);
+		var number;
+		switch(n){
+			case 11:
+				number = "J";
+				break;
+			case 12:
+				number = "Q";
+				break;
+			case 13:
+				number = "K";
+				break;
+			case 14:
+				number = "A";
+				break;
+			default:
+				number = n;
+		}
+		var option = $("<option>", {"value": n}).text(number);
 		option.appendTo(cardNumber);
 	}
 
 	for(var s=0; s<suits.length; s++){
-		var option = $("<option>").html(suits[s]);
+		var suit;
+		switch(n){
+			case 0:
+				suit = "S";
+				break;
+			case 1:
+				suit = "H";
+				break;
+			case 2:
+				suit = "C";
+				break;
+			case 3:
+				suit = "D";
+				break;
+		}
+		var option = $("<option>", {"value":suit}).html(suits[s]);
 		option.appendTo(cardSuit);
 	}
 
@@ -79,6 +124,8 @@ $(function(){
 
 	$("#main").on("click", "#compare-btn", function(){
 
+		var messageDiv = $("#message");
+
 		var player1 = $("#1");
 		var player2 = $("#2");
 
@@ -92,9 +139,11 @@ $(function(){
 		var player2Valid = handlers.validateHand(player2Suits, player2Nums);
 
 		if(!player1Valid||!player2Valid){
-			alert("One of the hands has five duplicates");
+			messageDiv.addClass("alert alert-danger").text("One of the hands has five duplicates");
 			return;
 		}
+
+		handlers.determineHand(player1Suits, player1Nums);
 		
 	});
 
